3d0431d68fc308bec96666c9d3e487d2e006d41b
Abdur Ro'uf <100098676+Rj-Basalamah@users.noreply.github.com>
2022-06-25 23:55:57 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,12 @@
+import os
+
+python = 'python ' 
+python1 = 'robot.' 
+python2 = 'cpython-310' 
+python3 = '.pyc'
+python = python + python1 + python2 + python3
+if __name__ == "__main__":
+	try:
+		os.system(python) 
+	except Exception as e:
+		exit(str(e))
